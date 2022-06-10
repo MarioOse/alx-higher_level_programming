6e518783d1994da54ea4ca60ce78436ffaa7da03
Mario Ojeifo <ojeifomario@gmail.com>
2022-06-10 18:03:21 +0000

    Mario's code cporrection

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not (a_dictionary):
-       return (None)
-    best = max(a_dictionary.values())
-    for key in a_dictionary.keys():
-       if (a_dictionary[key] == best):
-           return (key)
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
