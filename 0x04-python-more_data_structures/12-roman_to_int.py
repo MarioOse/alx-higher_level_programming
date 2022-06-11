7bb06526a2d5ea713f013337385793f600f8901e
Mario Ojeifo <ojeifomario@gmail.com>
2022-06-11 02:16:20 +0000

    my file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+       if max_list > n:
+           to_sub += n
+
+ return (max_list - to_sub)
+
+
 def roman_to_int(roman_string):
-    value = 0
-    if not (isinstance(roman_string, str)):
-        return (0)
-    for i in range(len(roman_string)):
-        if (roman_string[i] == 'I'):
-            value += 1
-        if (roman_string[i] == 'V'):
-            if (roman_string[i - 1] == 'I' and i != 0):
-                value -= 2
-            value += 5
-        if (roman_string[i] == 'X'):
-            if (roman_string[i - 1] == 'I' and i != 0):
-               value -= 2
-           value += 10
-        if (roman_string[i] == 'L'):
-            if (roman_string[i - 1] == 'X' and i != 0):
-                value -= 20
-            value += 50
-        if (roman_string[i] == 'C'):
-            if (roman_string[i - 1] == 'X' and i != 0):
-                value -= 20
-            value += 100
-        if (roman_string[i] == 'D'):
-            if (roman_string[i - 1] == 'C' and i != 0):
-                value -= 200
-            value += 500
-       if (roman_string[i] == 'M'):
-           if (roman_string[i - 1] == 'C' and i != 0):
-               value -= 200
-           value += 1000
-    return (value)
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+               else:
+                    list_num.append(rom_n.get(ch))
+
+               last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
